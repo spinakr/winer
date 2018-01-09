0d5b4faf5a192413be477e7dd10c0e03db080358
Anders Kofoed <anders.kfd@gmail.com>
2018-01-09 21:16:23 +0100

    Exposure wines from db through api

---
+++ src/api/Controllers/WineController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/wine")]
+    public class WineController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            using (var db = new SqlConnection(@"Server=localhost,1401;Database=winer;User Id=SA; Password=Qwer1234*;"))
+            {
+                var wines = db.GetList<Wine>();
+
+                return Ok(wines);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"her is {id}";
+        }
+    }
+}
